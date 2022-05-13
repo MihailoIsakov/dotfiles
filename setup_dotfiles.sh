9bacac28817c65792b11744d9c7b43a3aa71e6e2
Mihailo Isakov <misakov1@asu.edu>
2022-05-12 22:37:28 -0700

    kitty config

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,16 +1,17 @@
 chmod +x bin/*
 
-ln -s -f $(pwd)/bin/                   ~/
-ln -s -f $(pwd)/dotfiles/.profile      ~/
-ln -s -f $(pwd)/dotfiles/.bashrc       ~/
-ln -s -f $(pwd)/dotfiles/.vimrc        ~/
-ln -s -f $(pwd)/dotfiles/.sshrc        ~/
-ln -s -f $(pwd)/dotfiles/.xinitrc      ~/
-ln -s -f $(pwd)/dotfiles/.inputrc      ~/
-ln -s -f $(pwd)/dotfiles/.bash_git     ~/
-ln -s -f $(pwd)/dotfiles/.bash_aliases ~/
-ln -s -f $(pwd)/dotfiles/.flake8       ~/
-ln -s -f $(pwd)/dotfiles/.kitty.conf   ~/.config/kitty/
+ln -s -f $(pwd)/bin/                        ~/
+ln -s -f $(pwd)/dotfiles/.profile           ~/
+ln -s -f $(pwd)/dotfiles/.bashrc            ~/
+ln -s -f $(pwd)/dotfiles/.vimrc             ~/
+ln -s -f $(pwd)/dotfiles/.sshrc             ~/
+ln -s -f $(pwd)/dotfiles/.xinitrc           ~/
+ln -s -f $(pwd)/dotfiles/.inputrc           ~/
+ln -s -f $(pwd)/dotfiles/.bash_git          ~/
+ln -s -f $(pwd)/dotfiles/.bash_aliases      ~/
+ln -s -f $(pwd)/dotfiles/.flake8            ~/
+ln -s -f $(pwd)/dotfiles/kitty.conf         ~/.config/kitty/
+ln -s -f $(pwd)/dotfiles/current-theme.conf ~/.config/kitty/
 
 mkdir -p ~/.config/i3/
-ln -s -f $(pwd)/dotfiles/i3_config     ~/.config/i3/config
+ln -s -f $(pwd)/dotfiles/i3_config          ~/.config/i3/config
