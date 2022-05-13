c162752a2a90aa7bd1f8bd4f74609807dda4dfd6
Mihailo Isakov <misakov1@asu.edu>
2022-05-12 18:10:16 -0700

    comments

--- setup.sh
+++ setup.sh
@@ -1,15 +1,26 @@
 sudo apt install vim-gtk nmap i3 arandr rofi python3-pip bash-completion flake8
 # sudo apt install nvidia-driver-495
 
+###################################################
+# Use pipx or virtualenvwrapper whenever possible #
+###################################################
 pip install virtualenv virtualenvwrapper pipx
+
+#############################################
+# Setup a ~/tools/ directory for all clones #
+#############################################
+mkdir -p ~/tools/
+cd ~/tools
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+###############################
+# Require manual installation #
+###############################
 
 # Google Chrome installed from DPKdG
 # Fira Code 
 # Kitty
-
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
 
 ##################
 # Setup dotfiles #
