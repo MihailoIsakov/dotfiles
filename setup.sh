1bff29053d3b6e60102637c0fe7f87500ae6858f
Mihailo Isakov <misakov1@asu.edu>
2022-05-12 17:21:38 -0700

    dotfiles start

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt install vim-gtk nmap i3 arandr rofi python3-pip bash-completion flake8
+# sudo apt install nvidia-driver-495
+
+pip install virtualenv virtualenvwrapper pipx
+
+# Google Chrome installed from DPKdG
+# Fira Code 
+# Kitty
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
