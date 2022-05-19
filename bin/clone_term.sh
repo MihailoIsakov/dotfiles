5feab291623c6b5f73f733d1acf257f00eb55abb
mihailo <misakov1@asu.edu>
2022-05-19 03:21:57 -0700

    cloneterm fix

--- bin/clone_term.sh
+++ bin/clone_term.sh
@@ -2,11 +2,11 @@
 
 if [ $# -eq 0 ]; then
     #nohup gnome-terminal --working-directory="$PWD" >&/dev/null
-    nohup kitty --directory="$PWD" >&/dev/null
+    nohup kitty --directory="$PWD" >&/dev/null &
 else
     for (( c=1; c<=$1; c++ )) 
     do
         #nohup gnome-terminal --working-directory="$PWD" >&/dev/null
-        nohup kitty --directory="$PWD" >&/dev/null
+        nohup kitty --directory="$PWD" >&/dev/null &
     done
 fi
