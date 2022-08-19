710311e5cba72a9ccc1c53a0a54568468aaa3dbf
mihailo <misakov1@asu.edu>
2022-08-19 12:56:49 -0500

    instructions for triggering fastkeys on every time the keyboard is plugged in

--- bin/fastkeys.sh
+++ bin/fastkeys.sh
@@ -1,5 +1,9 @@
 #! /bin/bash 
 
+# Since fastkeys.sh needs to be run every time a keyboard is plugged in, 
+# put the following line into /etc/udev/rules.d/iris_keyboard.rules:
+#   ATTRS{idVendor}=="cb10", ATTRS{idProduct}=="2256", RUN+="/usr/bin/su mihailo -c \"DISPLAY=:0 XAUTHORITY=/home/mihailo/.Xauthority /home/mihailo/bin/fastkeys.sh"
+
 # faster keyboard
 xset r rate 180 60
 
