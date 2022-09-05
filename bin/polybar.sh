956080ff224432fc06fe1a0b065ea68657304b84
mihailo <misakov1@asu.edu>
2022-09-04 20:52:27 -0700

    framework

--- bin/polybar.sh
+++ bin/polybar.sh
@@ -1,8 +1,5 @@
 #! /bin/sh
 
 killall polybar
-MONITOR="DP-1"   polybar main &
-MONITOR="DP-2"   polybar main &
-MONITOR="DP-3"   polybar main &
-MONITOR="HDMI-1" polybar main &
+MONITOR="eDP-1"   polybar main
 
