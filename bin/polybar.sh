f6cf0c4df13181cfe1e6ab3d34a6ef7156484f26
Mihailo Isakov <isakov.m@gmail.com>
2022-09-26 14:27:15 -0700

    more generic polybar

--- bin/polybar.sh
+++ bin/polybar.sh
@@ -1,5 +1,15 @@
-#! /bin/sh
+#! /bin/bash
 
 killall polybar
-MONITOR="eDP-1"   polybar main
+
+if [[ $(hostname -s) = zver ]]; 
+then
+    MONITOR="DP-1"   polybar main &
+    MONITOR="DP-2"   polybar main &
+    MONITOR="DP-3"   polybar main &
+    MONITOR="HDMI-1" polybar main &
+elif [[ $(hostname -s) = framework ]]; 
+then
+    MONITOR="eDP-1"   polybar main
+fi
 
