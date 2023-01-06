a7c12c7a726a97c965da2bcf523b04de973a02fd
Mihailo Isakov <isakov.m@gmail.com>
2023-01-05 20:25:19 -0700

    polybar on all monitors

--- bin/polybar.sh
+++ bin/polybar.sh
@@ -2,13 +2,8 @@
 
 killall polybar
 
-if [[ $(hostname -s) = mihailo-thin ]]; 
-then
-    MONITOR="DP-0"   polybar main &
-    MONITOR="DP-2"   polybar main &
-    MONITOR="DP-4"   polybar main &
-elif [[ $(hostname -s) = framework ]]; 
-then
-    MONITOR="eDP-1"   polybar main
-fi
-
+for MONITOR in $(xrandr -q | grep " connected" | cut -d" " -f1); do
+    export MONITOR
+    echo $MONITOR
+    polybar main &
+done
