34c10c2a8da9872c15ee7942b058c058e1708c4d
Mihailo Isakov <isakov.m@gmail.com>
2023-03-04 12:49:13 -0700

    scratchpad changes

--- bin/scratchpad.sh
+++ bin/scratchpad.sh
@@ -1,31 +1,36 @@
 #! /bin/bash
 
-if [[ $1 != "spotify" ]] ; then         
-    i3-msg [instance="spotify"] focus, scratchpad show;
+spotify=$(xdotool search --onlyvisible --class "spotify")
+chrome=$(xdotool search --onlyvisible --class "google-chrome")
+echo $chrome
+chrome=$(echo $chrome | xargs -n1 xprop -id | grep I3 | cut -d ' ' -f 3)
+obsidian=$(xdotool search --onlyvisible --class "obsidian" | head -n 1)
+todoist=$(xdotool search --onlyvisible --classname ".*todoist.*")
+
+echo $chrome
+
+if [[ spotify -ne 0 ]]; then
+    i3-msg "[class=Spotify] focus, scratchpad show"
+elif [[ $1 == "spotify" ]]; then
+    i3-msg "focus output DP-4; [instance=spotify] scratchpad show, fullscreen enable;"
 fi
 
-if [[ $1 != "todoist" ]] ; then         
-    i3-msg [instance="todoist"] focus, scratchpad show;
+if [[ chrome -ne 0 ]]; then
+    i3-msg "[class=Google-chrome] focus, scratchpad show"
+elif [[ $1 == "google-chrome" ]]; then
+    i3-msg "focus output DP-2; [class=Google-chrome] scratchpad show, fullscreen enable;"
 fi
 
-if [[ $1 != "google-chrome" ]] ; then         
-    i3-msg [instance="google-chrome"] focus, scratchpad show;
+if [[ obsidian -ne 0 ]]; then
+    i3-msg "[title=\".*obsidian.*\"] focus, scratchpad show;"
+    i3-msg "[title=\".*myobs.*\"] focus, scratchpad show;"
+elif [[ $1 == "obsidian" ]] ; then
+    i3-msg "focus output HDMI-0; [title=\".*obsidian.*\"] scratchpad show, fullscreen enable;"
+    i3-msg "focus output DP-4; [title=\".*myobs.*\"] scratchpad show, fullscreen enable;"
 fi
 
-if [[ $1 != "obsidian" ]] ; then         
-    i3-msg [instance="obsidian"] focus, scratchpad show;
+if [[ todoist -ne 0 ]]; then
+    i3-msg "[instance="todoist"] focus, scratchpad show"
+elif [[ $1 == "todoist" ]] ; then
+    i3-msg "focus output DP-4; [instance=todoist] scratchpad show, fullscreen enable;"
 fi
-
-i3-msg "focus output DP-2; [instance="$1"] scratchpad show, fullscreen enable;"
-
-# spotify=$(xdotool search --onlyvisible --class "spotify")
-# chrome=$(xdotool search --onlyvisible --class "google-chrome")
-# 
-# if [[ spotify -ne 0 ]]; then
-#     i3-msg "[class=spotify] focus, scratchpad show"
-# fi
-# 
-# if [[ chrome -ne 0 ]]; then
-#     i3-msg "[instance='google-chrome'] focus, scratchpad show"
-# fi
-
